14c9d8517664c39205c28400f06a6914e6988522
SusheelThapa <susheelthapa90@gmail.com>
2024-06-08 21:58:41 +0545

    feat: add best selling prodcut sections

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,17 @@ import NewArrival from "./components/NewArrival";
 import FlashSales from "./components/FlashSales";
 import Features from "./components/Features";
 import Banner from "./components/Banner";
-
+import BestSellingProducts from "./components/BestSellingProducts";
 
 export default function Home() {
   return (
     <>
       <Header />
       <FlashSales />
+      <BestSellingProducts />
       <Banner />
       <NewArrival />
-      <Features/>
+      <Features />
       <Footer />
     </>
   );
