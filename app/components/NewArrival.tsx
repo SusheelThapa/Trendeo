6e61339006fc23fc2e74c560538f6160a96a9758
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-06-08 20:27:44 +0545

    new arrivals completed

--- app/components/NewArrival.tsx
+++ app/components/NewArrival.tsx
@@ -1,61 +1,89 @@
 import Image from 'next/image';
 
 const NewArrival = () => {
-  return (
-    <section className="md:mx-28 my-10 max-w-[1170px]">
-      <h1 className="text-2xl mt-4">New Arrival</h1>
-      <div className="grid grid-cols-2 gap-4 ">
-        <div className="relative" style={{ height: '600px', width: '570px' }}>
-          <Image
-            src="/images/ps5.png"
-            alt="PS5"
-            layout="fill"
-            objectFit="contain"
-          />
-          <div className="absolute inset-0 flex items-center justify-center">
-            <span className="text-white">Your Text Here</span>
-          </div>
-        </div>
-        <div className="grid grid-rows-2 gap-5">
-          <div className="relative" style={{ height: '280px' }}>
-            <Image
-              src="/images/women_collections.png"
-              alt="Women Collections"
-              layout="fill"
-              objectFit="contain"
-            />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <span className="text-white">Your Text Here</span>
+    return (
+        <section className="mx-4 md:mx-28 my-10 flex flex-col justify-center ">
+            <h1 className="text-3xl font-bold mt-8 mb-12">New Arrival</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <div className="relative p-4 flex justify-center items-center bg-black rounded-md h-[280px] lg:h-[580px] lg:w-[570]">
+                    <div className="relative w-full h-full">
+                        <Image
+                            src="/images/ps5.png"
+                            alt="PS5"
+                            layout="fill"
+                            objectFit="contain"
+                            className="rounded-md md:p-10"
+                        />
+                    </div>
+                    <div className="absolute inset-0 flex items-end z-20">
+                        <div className="text-white p-10 md:w-1/2">
+                            <h2 className='text-2xl font-bold tracking-tight my-4'>PlayStation 5</h2>
+                            <p className='text-md font-normal mb-4'>Black and White version of the PS5 comming out on sale</p>
+                            <span className='text-lg font-medium hover:cursor-pointer border-b-2 border-white'>Shop now</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid md:grid-rows-2 gap-5">
+                    <div className="relative p-4 flex justify-center items-center bg-black rounded-md" style={{ height: '280px' }}>
+                        <div className="relative w-full h-full">
+                            <Image
+                                src="/images/women_collections.png"
+                                alt="Women Collections"
+                                layout="fill"
+                                objectFit="contain"
+                                className="rounded-md"
+                            />
+                        </div>
+                        <div className="absolute inset-0 flex items-end z-20">
+                            <div className="text-white p-10 md:w-1/2">
+                                <h2 className='text-2xl font-bold tracking-tight my-4 w-1/2 md:w-full'>Women's Collections</h2>
+                                <p className='text-md font-normal mb-4'>Featured woman collections that give you another vibe.</p>
+                                <span className='text-lg font-medium hover:cursor-pointer border-b-2 border-white'>Shop now</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="relative p-4 flex justify-center items-center bg-black rounded-md" style={{ height: '280px' }}>
+                            <div className="relative w-full h-full">
+                                <Image
+                                    src="/images/speaker.png"
+                                    alt="Speaker"
+                                    layout="fill"
+                                    objectFit="contain"
+                                    className="rounded-md md:p-10"
+                                />
+                            </div>
+                            <div className="absolute inset-0 flex items-end z-20">
+                                <div className="text-white p-10 ">
+                                    <h2 className='text-2xl font-bold tracking-tight my-1'>Speakers</h2>
+                                    <p className='text-md font-normal mb-1'>Amazon wireless speakers</p>
+                                    <span className='text-md font-medium hover:cursor-pointer border-b-2 border-white'>Shop now</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="relative p-4 flex justify-center items-center bg-black rounded-md" style={{ height: '280px' }}>
+                            <div className="relative w-full h-full">
+                                <Image
+                                    src="/images/perfume.png"
+                                    alt="Perfume"
+                                    layout="fill"
+                                    objectFit="contain"
+                                    className="rounded-md md:p-10"
+                                />
+                            </div>
+                            <div className="absolute inset-0 flex items-end z-20">
+                                <div className="text-white p-10 ">
+                                    <h2 className='text-2xl font-bold tracking-tight my-1'>Perfume</h2>
+                                    <p className='text-md font-normal mb-1'>GUCCI INTENSE OUD ESP</p>
+                                    <span className='text-md font-medium hover:cursor-pointer border-b-2 border-white'>Shop now</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="relative" style={{ height: '280px' }}>
-              <Image
-                src="/images/speaker.png"
-                alt="Speaker"
-                layout="fill"
-                objectFit="contain"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <span className="text-white">Your Text Here</span>
-              </div>
-            </div>
-            <div className="relative" style={{ height: '280px' }}>
-              <Image
-                src="/images/perfume.png"
-                alt="Perfume"
-                layout="fill"
-                objectFit="contain"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <span className="text-white">Your Text Here</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default NewArrival;
