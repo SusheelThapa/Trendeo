09e2583e0a4b2fd00a6476a3276c0143997171c4
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-06-08 21:34:34 +0545

    minor changes to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NewArrival from "./components/NewArrival";
+import Features from "./components/Features";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <Banner/>
     <NewArrival/>
-    {/* <Footer/> */}
+    <Features/>
+    <Footer/>
     </>
   );
 }
