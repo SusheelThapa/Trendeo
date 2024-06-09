b78f4137d649418bc46e6c19811fccb87e3fdc38
SauravKumarMahato <077bct081.subash@pcampus.edu.np>
2024-06-09 13:44:45 +0545

    added animation to hero section and added favicon

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
-import heroImage from "../../public/images/HeroImage.png"; // Adjust the path as necessary
+import heroImage from "../../public/images/HeroImage.png"; 
 import ShopByCategory from "./ShopByCategory";
+import PopupAnimation from "./common/PopupAnimation";
 
 const HeroSection = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row gap-4 mx-4 md:gap-14 md:mx-28">
-        <div className="hidden lg:block">
-          <ShopByCategory />
-        </div>
-        <div className="relative w-full h-[170px] md:h-[250px] lg:h-[500px] text-white mt-6 md:col-span-2">
-          <Image
-            src={heroImage}
-            alt="Apple Logo"
-            className=" absolute object-contain hover:cursor-pointer w-full h-auto"
-          />
-        </div>
+        <PopupAnimation>
+          <div className="hidden lg:block">
+            <ShopByCategory />
+          </div>
+        </PopupAnimation>
+
+          <div className="relative w-full h-[170px] md:h-[250px] lg:h-[500px] text-white mt-6 md:col-span-2">
+        <PopupAnimation>
+            <Image
+              src={heroImage}
+              alt="Apple Logo"
+              className=" absolute object-contain hover:cursor-pointer w-full h-auto"
+            />
+        </PopupAnimation>
+          </div>
       </div>
     </>
   );
